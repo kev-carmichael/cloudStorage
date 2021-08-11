7898c34351b931eb5522b0d966a807fb8ebe34f1
Kev Carmichael <kev.carmichael22@gmail.com>
2021-08-11 13:39:33 +0100

    created error.html page and controller/ErrorMessageController class

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/ErrorMessageController.java
@@ -0,0 +1,31 @@
+package com.udacity.jwdnd.course1.cloudstorage.controller;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorMessageController implements ErrorController {
+    @GetMapping("/error")
+    //credit mentor Yuri
+    public String showError(Model model){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication == null || authentication instanceof AnonymousAuthenticationToken){
+            model.addAttribute("login",true);
+            return "errorPage";
+        }
+        model.addAttribute("home",true);
+        return "errorPage";
+    }
+    
+    @Override
+    public String getErrorPath() {
+        return null;
+    }
+}
