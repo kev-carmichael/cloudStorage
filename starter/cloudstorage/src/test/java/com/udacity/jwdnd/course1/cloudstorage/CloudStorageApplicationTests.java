ce48024e2a6d3e05a7a9e60e62af6e5a736683b7
Kev Carmichael <kev.carmichael22@gmail.com>
2021-08-26 18:35:59 +0100

    added tests 1a and 1b to existing test class CloudStorageApplicationTests

--- starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CloudStorageApplicationTests {
 
@@ -39,10 +41,57 @@ class CloudStorageApplicationTests {
 		}
 	}
 
-	@Test
+	@Test  // keep this - is this one of the tests req'd?
 	public void getLoginPage() {
 		driver.get("http://localhost:" + this.port + "/login");
-		Assertions.assertEquals("Login", driver.getTitle());
+		assertEquals("Login", driver.getTitle());
 	}
+
+	//***************  My Tests start from here  **********************
+	@Test
+	public void _1a_testVerifyUnauthorisedUserOnlyAccessLoginAndSignUp(){
+		driver.get("http://localhost" + this.port + "/login");
+		assertEquals("Login", driver.getTitle());
+
+		driver.get("http://localhost" + this.port + "/signup");
+		assertEquals("Sign Up", driver.getTitle());
+
+		driver.get("http://localhost" + this.port + "/home");
+		assertEquals("Home", driver.getTitle());
+	}
+
+	@Test
+	public void _1b_testUserSignupLoginAccessHomePageLogOutAndVerifyHomePageNotAccessible(){
+		//SET USER VALUES
+		String firstName = "John";
+		String lastName = "Smith";
+		String username = "jsmith";
+		String password = "123";
+
+		//USER SIGN IN
+		driver.get("http://localhost:" + this.port + "/signup");
+		signupPage = new SignupPage(driver);  //removed redeclaration of SignupPage
+		signupPage.signup(firstName, lastName, username, password);
+
+		//USER LOG IN
+		driver.get("http://localhost:" + this.port + "/login");
+		loginPage = new LoginPage(driver); //removed redeclaration of LoginPage
+		loginPage.login(username, password);
+
+		//VERIFY HOME PAGE ACCESSIBLE
+		driver.get("http://localhost:" + this.port + "/home");
+		assertEquals("Home", driver.getTitle());
+
+		//USER LOG OUT
+		homePage = new HomePage(driver); //removed redeclaration of HomePage
+		homePage.logout();
+		assertEquals("Login", driver.getTitle()); //goes to Login page when press Log Out
+
+		//VERIFY HOME PAGE NO LONGER ACCESSIBLE
+		driver.get("http://localhost" + this.port + "/home");
+		assertNotEquals("Home", driver.getTitle());
+	}
+
+
 
 }
