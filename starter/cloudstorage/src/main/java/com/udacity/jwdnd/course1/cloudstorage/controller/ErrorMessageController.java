184ef0b4194a5853aa7d78eb2db3741205c3fc94
Kev Carmichael <kev.carmichael22@gmail.com>
2021-09-16 16:53:06 +0100

    added @GetMapping and error-500 to ErrorMessageController

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/ErrorMessageController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/ErrorMessageController.java
@@ -15,17 +15,18 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/error")
 public class ErrorMessageController implements ErrorController {
-    @RequestMapping("/error")
     //credit mentor Yuri
+    @GetMapping()
     public String showError(HttpServletRequest request){
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         if (status != null) {
             Integer statusCode = Integer.valueOf(status.toString());
             if (statusCode == HttpStatus.NOT_FOUND.value()) {
-                return "HTTP 404";
+                return "error-404";
             } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()){
-                return "Internal Server Error";
+                return "error-500";
             }
         }
         return "error";
@@ -33,6 +34,6 @@ public class ErrorMessageController implements ErrorController {
 
     @Override
     public String getErrorPath() {
-        return "/error";
+        return null;
     }
 }
